b766984413f38a85ea6c21be01ec8fe8212b840d
Jay Froment-Rudder <jfromentrudder@gmail.com>
2016-02-21 23:44:33 -0500

    halfway done

--- draw.c
+++ draw.c
@@ -5,7 +5,86 @@
 #include "display.h"
 #include "draw.h"
 
+
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  int x,y;
+  if( x0 >= 0 && x1 >= 0 && y0 >= 0 && y1 >= 0 ) {
+    x = x0;
+    y = y0;
+    if( x <= y ) {
+      int A, B, d;
+      A = y1 - y0;
+      B = -1*(x1 - x0);
+      d = 2*A + B;
+
+      while( x < x1 ){
+	plot( s, c, x, y );
+	if( d > 0 ) {
+	  y ++;
+	  d += 2*B;
+	}
+	x ++;
+	d += 2A;
+      }
+    } 
+    else {
+      int A, B, d;
+      A = y1 - y0;
+      B = -1*(x1 - x0);
+      d = 1 / 2 * A + B;
+
+      while( y < y1 ) {
+	plot( s, c, x, y );
+	if( d < 0 ) {
+	  x ++;
+	  d += 2*A;
+	}
+	y ++;
+	d += 2*B;
+      }
+    }
+  }
+
+  if( x0 < 0 && x1 < 0 && y0 < 0 && y1 < 0 ) {
+    x = x1;
+    y = y1;
+    if( x <= y ) {
+      int A, B, d;
+      A = y1 - y0;
+      B = -1*(x1 - x0);
+      d = 2*A + B;
+
+      while( x < x0 ){
+        plot( s, c, x, y );
+	if( d > 0 ) {
+          y ++;
+          d += 2*B;
+        }
+	x ++;
+	d += 2A;
+      } 
+    }
+    else {
+      int A, B, d;
+      A = y0 - y1;
+      B = -1*(x0 - x1);
+      d = 1 / 2* A + B;
+
+      while( y < y0 ) {
+	plot( s, c, x, y );
+	if( d <0 ) {
+          x ++;
+          d += 2*A;
+        }
+	y ++;
+	d += 2*B;
+      } 
+    }
+  }
+
+  if( x0 >= 0 && x1 >= 0 && y0 <= 0 && y1 < 0 ){
+    
+  }
 }
 
