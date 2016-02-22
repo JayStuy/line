74be7818a20a8168bd921c80e7dea853a0d38ad1
Jay Froment-Rudder <jfromentrudder@gmail.com>
2016-02-22 01:04:34 -0500

    doesn't work yet

--- draw.c
+++ draw.c
@@ -25,7 +25,7 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
 	  d += 2*B;
 	}
 	x ++;
-	d += 2A;
+	d += 2*A;
       }
     } 
     else {
@@ -46,13 +46,13 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     }
   }
 
-  if( x0 < 0 && x1 < 0 && y0 < 0 && y1 < 0 ) {
+  else if( x0 < 0 && x1 < 0 && y0 < 0 && y1 < 0 ) {
     x = x1;
     y = y1;
     if( x <= y ) {
       int A, B, d;
-      A = y1 - y0;
-      B = -1*(x1 - x0);
+      A = y0 - y1;
+      B = -1*(x0 - x1);
       d = 2*A + B;
 
       while( x < x0 ){
@@ -62,7 +62,7 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
           d += 2*B;
         }
 	x ++;
-	d += 2A;
+	d += 2*A;
       } 
     }
     else {
@@ -73,7 +73,7 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
 
       while( y < y0 ) {
 	plot( s, c, x, y );
-	if( d <0 ) {
+	if( d < 0 ) {
           x ++;
           d += 2*A;
         }
@@ -83,8 +83,78 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
     }
   }
 
-  if( x0 >= 0 && x1 >= 0 && y0 <= 0 && y1 < 0 ){
-    
+  else if( x0 >= 0 && x1 >= 0 && y0 <= 0 && y1 < 0 ){
+    x = x0;
+    y = y0;
+    if( x*-1 >= y ) {
+      int A, B, d;
+      A = y0 - y1;
+      B = -1*(x1 - x0);
+      d = 2*A + B;
+
+      while( x < x1 ){
+        plot( s, c, x, y );
+	if( d > 0 ) {
+          y --;
+          d += 2*B;
+        }
+	x ++;
+	d += 2*A;
+      } 
+    }
+    else {
+      int A, B, d;
+      A = y1 - y0;
+      B = -1*(x1 - x0);
+      d = 1 / 2* A + B;
+
+      while( y > y1 ) {
+	plot( s, c, x, y );
+	if( d < 0 ) {
+          x ++;
+          d += 2*A;
+        }
+	y --;
+	d += 2*B;
+      } 
+    }
+  }
+
+  else {
+    x = x1;
+    y = y1;
+    if( x*-1 <= y ) {
+      int A, B, d;
+      A = y1 - y0;
+      B = -1*(x0 - x1);
+      d = 2*A + B;
+
+      while( x > x1 ){
+        plot( s, c, x, y );
+        if( d > 0 ) {
+          y ++;
+          d += 2*B;
+        }
+        x --;
+        d += 2*A;
+      }
+    }
+    else {
+      int A, B, d;
+      A = y1 - y0;
+      B = -1*(x0 - x1);
+      d = 1 / 2* A + B;
+
+      while( y < y1 ) {
+        plot( s, c, x, y );
+        if( d < 0 ) {
+          x --;
+          d += 2*A;
+        }
+        y ++;
+        d += 2*B;
+      }
+    }
   }
 }
 
